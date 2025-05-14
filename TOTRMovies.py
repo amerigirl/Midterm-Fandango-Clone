4f36afdc6f8e86269feeecd94c37ad7eeebe0348
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-05-13 19:03:28 -0700

    TOTRMovies initial setup

---
+++ TOTRMovies.py
@@ -0,0 +1,11 @@
+#Welcome to top of the rim movies
+
+def _print_welcome():
+    print("Welcome to Top of the Rim Movies" )
+    print("The place for all the BEST film has to offer!")
+
+
+
+if __name__ == "__main__":
+    _print_welcome()
+
