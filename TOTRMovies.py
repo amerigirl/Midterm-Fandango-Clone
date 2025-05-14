84a2762b1db7f73016de8f2dda5dfd57f75d0014
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-05-13 19:54:07 -0700

    updated flask and flask restful

--- TOTRMovies.py
+++ TOTRMovies.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with 
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+db = SQLAlchemy(app)
+api = Api(app)
+
 
 @app.route('/')
 def hello():
     return "Hello, Flask!"
 
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
