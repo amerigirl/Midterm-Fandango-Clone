779442c2fed9877ddf54ae33281fd5a03c922811
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-05-13 20:18:16 -0700

    environment set up

--- TOTRMovies.py
+++ TOTRMovies.py
@@ -5,7 +5,7 @@ from flask_restful import Resource, Api, reqparse, fields, marshal_with
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
-db = SQLAlchemy(app)
+
 api = Api(app)
 
 
--- movieModel.py
+++ movieModel.py
@@ -1,4 +1,17 @@
-class movie():
-    id
+import flask_sqlalchemy from SQLAlchemy
+
+db = SQLAlchemy()
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)  
+    release_date = db.Column(db.Integer, nullable=False)
+
+#allows for the creation of a new movie we can see!
+    def __repr__(self):
+        return f"<Movie {self.id} {self.title} {self.description} {self.release_date} {self.rating}>" 
 
  
